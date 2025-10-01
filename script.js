831e62a112490b05c01d642ae5d604f692f7bd79
Anna <annayuskiv15@gmail.com>
2025-10-01 16:23:52 +0200

    new

---
+++ script.js
@@ -0,0 +1,66 @@
+import './style.css';
+import fetchCountries from './fetchCountries';
+import { error, info } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import debounce from 'lodash.debounce';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  container: document.querySelector('.js-container'),
+};
+
+// Функції-шаблони
+function countryCardTpl(country) {
+  return `
+    <div class="country-card">
+      <h2>${country.name}</h2>
+      <img src="${country.flag}" alt="Прапор ${country.name}" width="200">
+      <p><b>Столиця:</b> ${country.capital}</p>
+      <p><b>Населення:</b> ${country.population}</p>
+      <p><b>Мови:</b> ${country.languages.map(l => l.name).join(', ')}</p>
+    </div>
+  `;
+}
+
+function countryListTpl(countries) {
+  return `
+    <ul class="country-list">
+      ${countries.map(c => `<li><img src="${c.flag}" width="50"> ${c.name}</li>`).join('')}
+    </ul>
+  `;
+}
+
+// Подія вводу з debounce
+refs.input.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+  const query = e.target.value.trim();
+
+  if (!query) {
+    refs.container.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(query)
+    .then(countries => {
+      refs.container.innerHTML = '';
+
+      if (countries.length > 10) {
+        info({ text: 'Занадто багато збігів. Уточніть пошук!' });
+        return;
+      }
+
+      if (countries.length >= 2 && countries.length <= 10) {
+        refs.container.innerHTML = countryListTpl(countries);
+        return;
+      }
+
+      if (countries.length === 1) {
+        refs.container.innerHTML = countryCardTpl(countries[0]);
+      }
+    })
+    .catch(() => {
+      error({ text: 'Країну не знайдено' });
+    });
+}
